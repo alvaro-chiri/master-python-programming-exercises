b96e335eba0b1f78ca6229837fc695ed40cf5c9f
Alvaro Chirinos <65077832+alvaro-chiri@users.noreply.github.com>
2023-10-13 01:33:03 +0000

    6.1 done

--- exercises/003-area_of_right_triangle/app.py
+++ exercises/003-area_of_right_triangle/app.py
@@ -1,7 +1,7 @@
 #Complete the function to return the area of the triangle.
 def area_of_triangle(arg1, arg2):
     #your code here, please remove the "None" 
-    return None
+    return 0.5*arg1*arg2
 
 # Testing your function
 print(area_of_triangle(3, 5))
--- exercises/006.1-square_value_of_number/app.py
+++ exercises/006.1-square_value_of_number/app.py
@@ -1,5 +1,5 @@
 # your code here
 def square(num):
-    return None
+    return num**2
 
 print(square(6))
